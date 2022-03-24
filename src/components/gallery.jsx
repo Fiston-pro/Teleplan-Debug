635d782f2cf6ccc40f5c5347f74b10e36bbede2f
Fiston <60503084+Fiston-pro@users.noreply.github.com>
2022-03-24 13:49:21 +0100

    Update gallery.jsx

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -7,8 +7,7 @@ export const Gallery = (props) => {
         <div className='section-title'>
           <h2>Gallery</h2>
           <p>
-            Lorem ips amet, consectetur adipiscing elit duis sed
-            dapibus leonec.
+            I would put some details here
           </p>
         </div>
         <div className='row'>
